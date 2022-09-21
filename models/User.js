eba38669a35ce050a1528af3f615603c9a9d6091
KeeveRW11 <keevewhyte+github@gmail.com>
2022-09-20 22:32:37 -0400

    create api routes

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//Creates User Model
+class User extends Model {}
+
+//initialize the model's data and configuration, 
+//passing in two objects as arguments.provides context
+//as to how those inherited methods should work.
+User.init(
+    {
+    // Define an ID column
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4]
+        }
+    }
+    },
+    {
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: 'user'  
+    }
+);
+
+module.exports = User;
